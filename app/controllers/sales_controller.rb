23cc88b3030c7bf30220717f1db01f48eb85650b
alexsantosfilho <devalexsandro@gmail.com>
2021-03-17 11:20:30 -0300

    feat: new sale

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -3,41 +3,40 @@ class SalesController < ApplicationController
 
   def index
     @sales = Sale.all
-    @total_sales = @sales.map { |data| data[:unit_price].to_f * data[:quantity].to_f }.reduce(0){|sum, x| sum + x}
+    @total_sales = @sales.map { |data| data[:unit_price].to_f * data[:quantity].to_f }.reduce(0) { |sum, x| sum + x }
   end
-  
-	def new_sale params
+
+  def new_sale(params)
     @sale = Sale.new(sale_params)
     @sale.unit_price = params[:unit_price]
     @sale.description = params[:description]
     @sale.quantity = params[:quantity]
     @sale.customer = Customer.find_or_create_by(name: params[:customer_name])
     @sale.address =  Address.find_or_create_by(name: params[:address])
-    @sale.vendor =  Vendor.find_or_create_by(name: params[:vendor_name])
+    @sale.vendor = Vendor.find_or_create_by(name: params[:vendor_name])
     @sale.save!
-    rescue
-      flash[:error] = "Error ao salvar"  
-	end
+  rescue StandardError
+    flash[:error] = 'Error ao salvar'
+  end
 
   def import
     unless sale_upload_params[:file]
       redirect_to :root
-      flash[:notice] = "Você deve escolher um arquivo"
-      return      
+      return
     end
     uploaded_file = sale_upload_params[:file]
     content_file = uploaded_file.read.force_encoding('UTF-8')
 
-    if content_file.blank? 
+    if content_file.blank?
       redirect_to :root
-      flash[:error] = 'arquivo sem dados' 
+      flash[:error] = 'arquivo sem dados'
       return
     end
 
     unless content_file.each_line.first.split(/\t/).size == 6
       redirect_to :root
       flash[:error] = 'Error no formato'
-      return            
+      return
     end
 
     rows = content_file.to_s.split(/\n/)
@@ -47,17 +46,17 @@ class SalesController < ApplicationController
     process_file.each do |params|
       new_sale(params)
     end
- 
-    redirect_to sales_path, flash: {notice: 'importado com sucesso'}
-  end  
 
-  private 
-    def sale_upload_params
-      params.permit(:file)
-    end
+    redirect_to sales_path, flash: { notice: 'importado com sucesso' }
+  end
 
-    def sale_params
-      params.permit(:customer_name, :vendor_name, :address)
-    end
+  private
 
+  def sale_upload_params
+    params.permit(:file)
+  end
+
+  def sale_params
+    params.permit(:customer_name, :vendor_name, :address)
+  end
 end
